5d12836f9d28c85ea861eb763533d52daac91106
swalana <5ia17saott@gibwintra.ch>
2018-11-05 07:59:18 +0100

    remove useless tabs
    Committer: swalana <5ia17saott@gibwintra.ch>

--- src/application/Calculator.java
+++ src/application/Calculator.java
@@ -93,4 +93,33 @@ public class Calculator {
 		return i*r;
 	}
 	
+	/**
+	 * calculate u from p and i
+	 * @param p
+	 * @param i
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	private double uFromPAndI(double p, double i) throws IllegalArgumentException {
+		if (i==0.0){
+			throw new IllegalArgumentException("Argument 'Current' is 0");
+		}
+		return p/i;
+	}
+	
+	/**
+	 * calculate u from p and r
+	 * @param p
+	 * @param r
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	private double uFromPAndR(double p, double r) throws IllegalArgumentException {
+		if ((p*r)<0){
+			throw new IllegalArgumentException("Argument 'Power * Resistance' is < 0");
+		}
+		return Math.sqrt(p*r);
+	}
+	
+	
 }
