73c4c0bd41c2ae8eb74f1402b4e106dd1c635f0b
joelwaeger <5ia17jowaeger@gibwintra.ch>
2018-11-26 09:09:10 +0100

    fixed the feature for the red highlight

--- src/application/Main.java
+++ src/application/Main.java
@@ -44,7 +44,6 @@ public class Main extends Application {
 			TextField txLeistung = new TextField();
 			txLeistung.relocate(100, 285);
 			txLeistung.setFont(Font.font("Verdana", 15));
-			txLeistung.setStyle("- fx-text-fill = red;");
 			root.getChildren().add(txLeistung);
 
 			Label lblSpannung = new Label("Spannung:");
@@ -55,7 +54,6 @@ public class Main extends Application {
 			TextField txSpannung = new TextField();
 			txSpannung.relocate(100, 325);
 			txSpannung.setFont(Font.font("Verdana", 15));
-			txSpannung.setStyle("- fx-text-fill = red;");
 			root.getChildren().add(txSpannung);
 
 			Label lblStrom = new Label("Strom:");
@@ -66,7 +64,6 @@ public class Main extends Application {
 			TextField txStrom = new TextField();
 			txStrom.relocate(100, 365);
 			txStrom.setFont(Font.font("Verdana", 15));
-			txStrom.setStyle("- fx-text-fill = red;");
 			root.getChildren().add(txStrom);
 
 			Label lblWiderstand = new Label("Widerstand:");
@@ -77,7 +74,6 @@ public class Main extends Application {
 			TextField txWiderstand = new TextField();
 			txWiderstand.relocate(100, 405);
 			txWiderstand.setFont(Font.font("Verdana", 15));
-			txWiderstand.setStyle("- fx-text-fill = red;");
 			root.getChildren().add(txWiderstand);
 
 			Button btnBerechnen = new Button();
@@ -86,11 +82,15 @@ public class Main extends Application {
 			root.getChildren().add(btnBerechnen);
 			
 			btnBerechnen.setOnAction(e -> {
+				
+			
 				Calculator myCalculator = new Calculator(
-						Double.parseDouble(txLeistung.getText()),
-						Double.parseDouble(txSpannung.getText()),
-						Double.parseDouble(txStrom.getText()),
+				
+						Double.parseDouble(txLeistung.getText());
+						Double.parseDouble(txSpannung.getText());
+						Double.parseDouble(txStrom.getText());
 						Double.parseDouble(txWiderstand.getText()));
+						
 				System.out.print("Vorher:  ");
 				System.out.println(myCalculator.toString());
 				myCalculator.calculate();
