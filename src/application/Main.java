4d2994308c4de444b430323c49742cdc43753517
joelwaeger <5ia17jowaeger@gibwintra.ch>
2018-11-26 07:58:41 +0100

    add the feature for highlight the results red

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,7 @@ package application;
 
 import java.io.FileInputStream;
 
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -38,10 +39,12 @@ public class Main extends Application {
 			lbleistung.relocate(10, 285);
 			lbleistung.setFont(Font.font(15));
 			root.getChildren().add(lbleistung);
-
+			
+			
 			TextField txLeistung = new TextField();
 			txLeistung.relocate(100, 285);
 			txLeistung.setFont(Font.font("Verdana", 15));
+			txLeistung.setStyle("- fx-text-fill = red;");
 			root.getChildren().add(txLeistung);
 
 			Label lblSpannung = new Label("Spannung:");
@@ -52,6 +55,7 @@ public class Main extends Application {
 			TextField txSpannung = new TextField();
 			txSpannung.relocate(100, 325);
 			txSpannung.setFont(Font.font("Verdana", 15));
+			txSpannung.setStyle("- fx-text-fill = red;");
 			root.getChildren().add(txSpannung);
 
 			Label lblStrom = new Label("Strom:");
@@ -62,6 +66,7 @@ public class Main extends Application {
 			TextField txStrom = new TextField();
 			txStrom.relocate(100, 365);
 			txStrom.setFont(Font.font("Verdana", 15));
+			txStrom.setStyle("- fx-text-fill = red;");
 			root.getChildren().add(txStrom);
 
 			Label lblWiderstand = new Label("Widerstand:");
@@ -72,6 +77,7 @@ public class Main extends Application {
 			TextField txWiderstand = new TextField();
 			txWiderstand.relocate(100, 405);
 			txWiderstand.setFont(Font.font("Verdana", 15));
+			txWiderstand.setStyle("- fx-text-fill = red;");
 			root.getChildren().add(txWiderstand);
 
 			Button btnBerechnen = new Button();
